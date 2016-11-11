22644b750d762bea42ae1758ddcd87487821b9e2
Kyle O'Brien <kyledevinobrien1@gmail.com>
2016-11-11 13:24:23 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+$(document).ready(function () {
+
+	$("#menu-close").click(function (e) {
+		e.preventDefault();
+		$("#sidebar-wrapper").toggleClass("active");
+	});
+
+	// Opens the sidebar menu
+	$("#menu-toggle").click(function (e) {
+		e.preventDefault();
+		$("#sidebar-wrapper").toggleClass("active");
+	});
+
+	// Scrolls to the selected menu item on the page
+	$(function () {
+		$('a[href*=#]:not([href=#])').click(function () {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+
+	$(window).scroll(function () {
+		$('.hideme').each(function (i) {
+			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			if (bottom_of_window > (bottom_of_object * 0.85)) {
+				$(this).animate({
+					'opacity': '1'
+				}, 3000);
+			}
+		});
+	});
+
+	$("#summaryClick").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#bio").offset().top
+		}, 2000);
+	});
+	$("#skillsClick").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#experience").offset().top
+		}, 2000);
+	});
+	$("#projectsClick").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#projects").offset().top
+		}, 2000);
+	});
+	$("#contactClick").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#contact").offset().top
+		}, 2000);
+	});
+  $("#arrow").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#bio").offset().top
+		}, 2000);
+	});
+
+	/*!
+	 * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+	 * Code licensed under the Apache License v2.0.
+	 * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+	 */
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+	    $('a.page-scroll').bind('click', function(event) {
+	        var $anchor = $(this);
+	        $('html, body').stop().animate({
+	            scrollTop: $($anchor.attr('href')).offset().top
+	        }, 1500, 'easeInOutExpo');
+	        event.preventDefault();
+	    });
+	});
+
+	// Highlight the top nav as scrolling occurs
+	$('body').scrollspy({
+	    target: '.navbar-fixed-top'
+	})
+
+	// Closes the Responsive Menu on Menu Item Click
+	$('.navbar-collapse ul li a').click(function() {
+	    $('.navbar-toggle:visible').click();
+	});
+
+  $(".header-text").delay(700).animate({
+    opacity: 1
+  }, 700);
+
+
+});
