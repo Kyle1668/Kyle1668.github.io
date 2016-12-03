c13a62013b4cf47aef2f7faa49123fcd982ffeb6
Kyle1668 <kyledevinobrien1>
2016-12-03 00:51:37 -0800

    Syntax Fixes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-	// Fade in Effect
-	$(window).scroll(function () {
-		$('.hideme').each(function (i) {
+    // Fade in Effect
+    $(window).scroll(function() {
+        $('.hideme').each(function(i) {
 
-			// Screen Position
-			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-			var bottom_of_window = $(window).scrollTop() + $(window).height();
+            // Screen Position
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-			if (bottom_of_window > (bottom_of_object * 0.85)) {
-				$(this).animate({
-					'opacity': '1'
-				}, 3000);
-			}
-		});
-	});
+            if (bottom_of_window > (bottom_of_object * 0.85)) {
+                $(this).animate({
+                    'opacity': '1'
+                }, 3000);
+            }
+        });
+    });
 
 });
